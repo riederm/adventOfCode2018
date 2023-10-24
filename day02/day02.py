531451377c7ce4e30eb3a609809a3a9759ed2397
bLeadDev <59748268+bLeadDev@users.noreply.github.com>
2023-10-24 16:40:37 +0000

    Implemented first task on 2nd day

--- day02/day02.py
+++ day02/day02.py
@@ -12,6 +12,27 @@ def read_input(file_name):
         print(f"An error occurred: {e}")
     return lines
 
-#a
-lines = read_input('input.txt')
+
+lines = read_input('input1.txt')
 # solve the problem here!
+
+count_2_same = 0
+count_3_same = 0
+
+for line in lines:
+    char_count = {}
+    
+    for char in line:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    if 2 in char_count.values():
+        count_2_same += 1
+    if 3 in char_count.values():
+        count_3_same += 1
+
+result = count_2_same * count_3_same
+
+print(f"Result: {result}")
